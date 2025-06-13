0aae5e95c9c31215840e18d94c00250689272ea6
AndriySt92 <andriyua-1992@mail.ru>
2025-06-13 13:39:29 +0300

    chore(tailwind): configure custom theme with colors, fonts, and container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#000',
+        secondary: '#fff',
+        accent: '#1a00ff',
+      },
+      fontFamily: {
+        title: ['Montserrat', 'sans-serif'],
+        body: ['HelveticaNeueCyr', 'sans-serif'],
+      },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+        },
+        center: true,
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+          '3xl': '1720px',
+        },
+      },
+    },
   },
   plugins: [],
 };
