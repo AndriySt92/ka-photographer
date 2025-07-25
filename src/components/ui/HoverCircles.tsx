b2ef1c3f2d66153121675676373a02c407b1dca1
AndriySt92 <andriyua-1992@mail.ru>
2025-07-25 20:59:31 +0300

    style(components): update styles HoverCircles, add className props

--- src/components/ui/HoverCircles.tsx
+++ src/components/ui/HoverCircles.tsx
@@ -1,17 +1,18 @@
 import { motion } from 'framer-motion';
 
 import { logo } from '../../assets/icons';
-import { circleVariants, staggerContainer } from '../../lib';
+import { circleVariants, cn, staggerContainer } from '../../lib';
 
-interface CirclesProps {
+interface HoverCirclesProps {
   withLogo?: boolean;
+  className?: string;
 }
 
-const HoverCircles = ({ withLogo = false }: CirclesProps) => {
+const HoverCircles = ({ withLogo = false, className }: HoverCirclesProps) => {
   return (
-    <div className="pointer-events-none absolute -top-[1.1%] left-0 z-0 h-[102%] w-full">
+    <div className={cn('pointer-events-none absolute inset-0 z-0 h-full w-full', className)}>
       <motion.div
-        className="pointer-events-none absolute inset-0 flex items-center justify-center"
+        className="pointer-events-none absolute inset-0 my-1 flex items-center justify-center"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.2 }}
@@ -19,26 +20,26 @@ const HoverCircles = ({ withLogo = false }: CirclesProps) => {
       >
         {/* Outer circle - largest */}
         <motion.div
-          className="pointer-events-auto absolute -z-[3] aspect-square h-[110%] rounded-full border border-secondary/30 transition-all duration-700 hover:bg-secondary/20 group-hover:bg-secondary/20"
+          className="pointer-events-auto -z-[3] hidden aspect-square h-full rounded-full border border-secondary/30 transition-all duration-700 hover:bg-secondary/20 group-hover:bg-secondary/20 sm:block"
           variants={circleVariants}
           custom={5}
         />
 
         {/* Middle circle */}
         <motion.div
-          className="pointer-events-auto absolute -z-[2] aspect-square h-[70%] rounded-full border border-secondary/30 bg-primary transition-all duration-500 hover:bg-secondary/10"
+          className="pointer-events-auto absolute -z-[2] aspect-square h-[64%] rounded-full border border-secondary/30 bg-primary transition-all duration-500 hover:bg-secondary/10"
           variants={circleVariants}
           custom={4}
         />
 
         {/* Inner circle - smallest */}
         <motion.div
-          className="pointer-events-auto absolute -z-[1] flex aspect-square h-1/3 items-center justify-center rounded-full border border-secondary/40 bg-primary"
+          className="pointer-events-auto absolute -z-[1] flex aspect-square h-[30%] items-center justify-center rounded-full border border-secondary/40 bg-primary"
           variants={circleVariants}
           custom={1}
         >
           {withLogo && (
-            <div className="flex h-[60%] w-[60%] items-center justify-center duration-300 hover:scale-105 hover:opacity-60">
+            <div className="hidden h-[60%] w-[60%] items-center justify-center duration-300 hover:scale-105 hover:opacity-60 sm:flex">
               <img src={logo} alt="logo" className="object-contain" />
             </div>
           )}
