0f8a0e13c76c5eccd47f635b62f5e38022866fbd
AndriySt92 <andriyua-1992@mail.ru>
2025-07-25 21:09:26 +0300

    style(coponents): add style prop in BackgroundGradient.tsx

--- src/components/ui/BackgroundGradient.tsx
+++ src/components/ui/BackgroundGradient.tsx
@@ -1,4 +1,4 @@
-import type { HTMLAttributes } from 'react';
+import type { CSSProperties, HTMLAttributes } from 'react';
 import { motion, type MotionProps } from 'framer-motion';
 
 import { cn } from '../../lib';
@@ -8,20 +8,22 @@ interface BackgroundGradientProps {
   gradient?: string;
   animated?: boolean;
   motionProps?: MotionProps & HTMLAttributes<HTMLDivElement>;
+  style?: CSSProperties;
 }
 
 const BackgroundGradient = ({
   className,
-  gradient = 'linear-gradient(90deg, #1a00ff 0%, #000 100%)',
+  gradient = 'linear-gradient(90deg, #1a00ff 0%, transparent 100%)',
   animated = false,
   motionProps = {},
+  style = {},
 }: BackgroundGradientProps) => {
   // Choose element types
   const WrapperComponent = animated ? motion.div : 'div';
   return (
     <WrapperComponent
-      className={cn(`z-5 absolute h-full w-full opacity-40`, className)}
-      style={{ background: gradient }}
+      className={cn('z-5 absolute h-full w-full opacity-40', className)}
+      style={{ background: gradient, ...style }}
       {...(animated ? motionProps : {})}
     />
   );
