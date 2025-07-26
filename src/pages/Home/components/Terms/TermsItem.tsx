e4d0b8268a4fe565cd5157d0e19dac432e0a6304
AndriySt92 <andriyua-1992@mail.ru>
2025-07-26 17:23:19 +0300

    refactor(Home): expose TermsItem.tsx, update styles in Terms section

---
+++ src/pages/Home/components/Terms/TermsItem.tsx
@@ -0,0 +1,50 @@
+import { motion } from 'framer-motion';
+
+import { BackgroundGradient, Typography } from '../../../../components/ui';
+import { cn, fadeInLeft, fadeInRight } from '../../../../lib';
+
+interface TermsItemProps {
+  item: { title: string; subtitle: string };
+  index: number;
+}
+
+const TermsItem = ({ item, index }: TermsItemProps) => {
+  const isEven = (index + 1) % 2 === 0;
+
+  return (
+    <motion.div
+      variants={isEven ? fadeInRight : fadeInLeft}
+      className="space-y-0 lg:space-y-2 xl:space-y-3 2xl:space-y-5"
+    >
+      <div className="relative flex items-center py-2 sm:py-1">
+        <BackgroundGradient
+          className={cn(
+            'h-full w-[90vw] sm:w-[60vw] lg:w-[38vw] [@media(min-width:1950px)]:w-[30vw]',
+            isEven
+              ? 'right-[calc(50%-50vw)] rotate-[180deg] bg-gradient-to-r from-[#1a00ff] to-transparent [@media(min-width:1950px)]:right-0'
+              : 'left-[calc(50%-50vw)] bg-gradient-to-l from-[#1a00ff] to-transparent [@media(min-width:1950px)]:left-0',
+          )}
+        />
+
+        <Typography
+          parentAs="h3"
+          size="5xl"
+          align={isEven ? 'right' : 'left'}
+          className="relative z-50 w-full uppercase leading-[1.1] sm:normal-case"
+        >
+          {item.title}
+        </Typography>
+      </div>
+
+      <Typography
+        parentAs="div"
+        size="lg"
+        content={item.subtitle.split('\n')}
+        align={isEven ? 'right' : 'left'}
+        className="relative z-40 normal-case text-secondary xl:uppercase"
+      />
+    </motion.div>
+  );
+};
+
+export default TermsItem;
--- src/pages/Home/components/Terms/index.tsx
+++ src/pages/Home/components/Terms/index.tsx
@@ -1,114 +1,43 @@
 import { motion } from 'framer-motion';
 
-import { BackgroundGradient, HoverCircles, Typography } from '../../../../components/ui';
+import { Typography } from '../../../../components/ui';
 import { terms } from '../../../../config';
-import { useWindowSize } from '../../../../hooks';
-import {
-  containerVariants,
-  fadeInBottom,
-  fadeInLeft,
-  fadeInRight,
-  staggerContainer,
-} from '../../../../lib';
+import { fadeInBottom, staggerContainer } from '../../../../lib';
+
+import TermsItem from './TermsItem';
 
 const Terms = () => {
-  const { width } = useWindowSize();
-
-  const getMarginValue = () => {
-    if (width > 2100) return 0;
-    if (width > 1800) return (width - 1672) / 2;
-    if (width > 1280) return (width - 1232) / 2;
-    if (width > 1024) return (width - 976) / 2;
-    if (width > 725) return (width - 590) / 2;
-    if (width > 360) return (width - 350) / 2;
-
-    return 0;
-  };
-
-  const marginValue = getMarginValue();
-
   return (
     <motion.div
-      className="relative"
+      className="pointer-events-none relative"
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      variants={containerVariants}
+      variants={staggerContainer()}
     >
-      <div className="pointer-events-none relative z-10 space-y-6 xl:space-y-8 2xl:space-y-10">
+      <div className="relative z-10 space-y-12 2xl:space-y-16">
         {/* Title */}
-        <Typography
-          parentAs="h1"
-          size="extraLarge"
-          content={['Умови', 'Співпраці']}
-          childrenClasses={{ 1: 'text-right ml-[12%]' }}
-          className="mx-auto w-fit !leading-[0.8] sm:mx-0"
-          animated
-          parentMotionProps={{
-            variants: staggerContainer(),
-          }}
-          childrenVariants={fadeInBottom}
-        />
+        <div className="flex justify-center sm:justify-start">
+          <Typography
+            parentAs="h1"
+            size="extraLarge"
+            content={['Умови', 'Співпраці']}
+            childrenClasses={{ 1: 'text-right ml-[12%]' }}
+            className="mr-[8%] !leading-[0.8] sm:mx-0"
+            animated
+            parentMotionProps={{
+              variants: staggerContainer(),
+            }}
+            childrenVariants={fadeInBottom}
+          />
+        </div>
 
-        <motion.div
-          className="space-y-12 xl:space-y-3 xl:pt-20 2xl:mb-24 2xl:space-y-5"
-          variants={staggerContainer()}
-        >
-          {terms.map((item, index) => {
-            const isEven = (index + 1) % 2 === 0;
-
-            return (
-              <motion.div
-                variants={isEven ? fadeInRight : fadeInLeft}
-                key={item.title}
-                className="space-y-1 lg:space-y-2 xl:space-y-3 2xl:space-y-5"
-              >
-                <div className="relative flex items-center py-3">
-                  {/* Gradient overlay */}
-                  <BackgroundGradient
-                    className="h-full w-full sm:w-[50%]"
-                    style={{
-                      left: !isEven ? 0 : '',
-                      right: isEven ? 0 : '',
-                      background: isEven
-                        ? 'linear-gradient(270deg, #1a00ff 0%, transparent 100%)'
-                        : 'linear-gradient(90deg, #1a00ff 0%, transparent 100%)',
-                      marginLeft: !isEven ? `-${marginValue}px` : 0,
-                      marginRight: isEven ? `-${marginValue}px` : 0,
-                    }}
-                  />
-
-                  <Typography
-                    parentAs="h3"
-                    size="5xl"
-                    align={isEven ? 'right' : 'left'}
-                    className="relative z-50 w-full uppercase leading-[0.77]"
-                  >
-                    {item.title}
-                  </Typography>
-                </div>
-
-                <Typography
-                  parentAs="div"
-                  size="xl"
-                  align={isEven ? 'right' : 'left'}
-                  className="relative z-40 normal-case text-secondary xl:uppercase"
-                >
-                  {item.subtitle.split('\n').map((line, i) => (
-                    <span key={i}>
-                      {line}
-                      <br />
-                    </span>
-                  ))}
-                </Typography>
-              </motion.div>
-            );
-          })}
+        <motion.div className="space-y-12 xl:space-y-3 2xl:space-y-5" variants={staggerContainer()}>
+          {terms.map((item, index) => (
+            <TermsItem key={item.title} item={item} index={index} />
+          ))}
         </motion.div>
       </div>
-
-      {/* Concentric Circles */}
-      <HoverCircles />
     </motion.div>
   );
 };
