71ead726d85c34e085b1f5e785acc478d7364891
AndriySt92 <andriyua-1992@mail.ru>
2025-09-09 20:45:22 +0300

    chore(config): add path alias @ for src directory in Vite and TS config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ka-photographer',
+  resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+  },
   css: {
     postcss: './postcss.config.js',
   },
