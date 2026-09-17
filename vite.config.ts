8a404f8df065e4c09c525715d7191244a5dbcae2
Andriy <aaandriy92@gmail.com>
2026-09-17 18:25:10 +0300

    chore: update Vite and TypeScript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,34 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const base = process.env.VITE_BASE ?? '/';
 
-export default defineConfig({
-  plugins: [react()],
-  base,
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      visualizer({
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      }),
+    ],
+
+    base: env.VITE_BASE || '/',
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  css: {
-    postcss: './postcss.config.js',
-  },
+
+    css: {
+      postcss: './postcss.config.js',
+    },
+  };
 });
